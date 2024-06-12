f77cee9c576b9d5ccdb8118061781c20ec37afe7
VnFranco1 <166458377+VnFranco1@users.noreply.github.com>
2024-06-12 11:10:01 -0300

    trabalho 06.py

---
+++ trabalho 06.py
@@ -0,0 +1,15 @@
+print ("oi professor")
+numero = 10
+nome = "Eduardo"
+booleano= True
+numero_real=10.0
+a=10
+b=5
+soma=a+b
+subtracao=a-b
+multiplicacao=a*b
+divisão=a/b
+
+# a_ordem_de_precedencia_logica_not_and_or
+# para_mudar_a_ordem_basta_seguir_ordem_de_acordo_com_a_avaliacao_de_maior_pecedencia_ate_o_de_menor_precedencia
+resposta = (2>3) or (4==5 and numero !=10)
